e12a7731d516c92036ccf7e4f4cc84c2dbefb3f4
cjmj216 <cjmj216@gmail.com>
2022-07-13 16:35:21 +0900

    수업예제

---
+++ Ch02/5_변환.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 2022/07/13
+ * 이름 조민제
+ * 데이터 령식 변환 실습하기 교재 p106
+ * 
+ */
+namespace Ch02
+{
+    internal class _5_변환
+    {
+        static void Main5(string[] args)
+        {
+            //작은 변수에서 큰 변수
+            byte a1 = 255; //1byet
+            short a2 = a1; //2byte
+            int a3 = a2;   //4byte
+
+            Console.WriteLine("a1 : {0}",a1);
+            Console.WriteLine("a2 : {0}",a2);
+            Console.WriteLine("a3 : {0}",a3);
+
+            //큰변수 에서 작은변수로 이동
+            int a4 = 256;
+            short a5 =(short) a4;  //Casting
+            byte a6 = (byte)a5;    //Casting
+
+            Console.WriteLine("a4:{0}",a4);
+            Console.WriteLine("a5:{0}",a5);
+            Console.WriteLine("a6:{0}",a6);
+
+            //정수에서 실수
+            int a = 1;
+            int b = 3;
+            int c= 2;
+
+            double d1 = a;
+            double d2 = b;
+            double d3 = c;
+
+            Console.WriteLine("d1:{0}",d1);
+            Console.WriteLine("d2:{0}",d2);
+            Console.WriteLine("d3:{0}",d3);
+
+            //실수에서 정수
+            double n1 = 1.2;
+            double n2 = 2.14;
+            double n3 = 12.123;
+
+            int r1 = (int)n1;
+            int r2 = (int)n2;
+            int r3 = (int)n3;
+
+            Console.WriteLine("r1:",+r1);
+            Console.WriteLine("r2:",+r2);
+            Console.WriteLine("r3:",+r3);
+        }
+    }
+}
---
+++ Ch02/6_문자열.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 문자열 실습하기 교재 p112
+ * 
+ * 
+ */
+namespace Ch02
+{
+    internal class _6_문자열
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning";
+            Console.WriteLine(greeting);
+            Console.WriteLine();
+
+            //문자열 길이
+            Console.WriteLine("길이:"+ greeting.Length);
+            Console.WriteLine();
+
+            //문자열 추출
+            Console.WriteLine("greeting 1번째 문자:" + greeting [1]);
+            Console.WriteLine("greeting 6번째 문자:" + greeting [5]);
+            Console.WriteLine("greeting 12번째 문자:" + greeting [11]);
+            Console.WriteLine();
+
+            //문자열 인덱스
+            Console.WriteLine("'G'인덱스:"+ greeting.IndexOf('G'));
+            Console.WriteLine("'m'인덱스:" +greeting.IndexOf('m'));
+            Console.WriteLine("'o'인덱스:" +greeting.IndexOf('o'));     //앞에서 부터
+            Console.WriteLine("'o'인덱스:" +greeting.LastIndexOf('o')); //뒤에서 부터
+            Console.WriteLine();
+
+            //문자열 자르기
+            Console.WriteLine("greeting 0~4 까지 문자열:"+greeting.Substring(0,4));
+            Console.WriteLine("greeting 5~7 까지 문자열:"+greeting.Substring(5,7));
+            Console.WriteLine("greeting 5 마지막까지 문자열:"+greeting.Substring(5));
+            Console.WriteLine();
+
+            //문자열 교체
+            string replaced = greeting.Replace("Morning", "Afternoon");
+            Console.WriteLine(replaced);
+            Console.WriteLine();
+
+            //문자열 변화
+            int v1 = 1;
+            double v2 = 2.12345;
+            bool v3 = true;
+
+            string str1 =v1.ToString();
+            string str2 = v2.ToString();
+            string str3 = "" + v3;
+
+            Console.WriteLine("str1:"+str1);
+            Console.WriteLine("str2:"+str2);
+            Console.WriteLine("str3:"+str3);
+            Console.WriteLine();
+            string s1 = "100";
+            string s2 = "3.14";
+            string s3 = "false";
+
+            int i1 =int.Parse (s1);
+            double i2 =double.Parse (s2);
+            bool i3 =bool.Parse (s3);
+
+            Console.WriteLine("i1:"+i1);
+            Console.WriteLine("i2:"+i2);
+            Console.WriteLine("i3:"+i3);
+
+
+
+           
+
+
+             
+        }
+    }
+}
