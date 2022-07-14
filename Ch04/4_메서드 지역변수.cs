b2440b3263041e24ce9a8893cd001d0a54d1d2e6
cjmj216 <cjmj216@gmail.com>
2022-07-14 11:42:57 +0900

    수업예저

---
+++ Ch04/4_메서드 지역변수.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 메서드 지역변수 실습하기 p
+ * 
+ * 전역변수 (Global Variable)
+ * -모든 메서드에서 참조할 수 있는변수
+ * -객체지향프로그래밍에서 전역변수는 멤버변수(속성)
+ * 지역변수 (Local Variable)
+ * -특정 메서드에서 선언한 변수로 해당 메서드에서만 참조한다
+ * -해당 메서드가 끝나면 Stack에서 사라진다(Pop)
+ */
+namespace Ch04
+{
+    internal class _4_메서드_지역변수
+    {
+        //전역변수
+        static int result=0;           //전역변수
+
+        static void Main1(string[] args)
+        {
+            //지역변수
+            int n1 = 1;
+            int n2 = 10;
+            result = Sum(n1, n2);
+            Console.WriteLine("result : "+result);
+
+
+        }//end of Mine
+
+        public static int Sum(int start, int end)
+        {
+
+            result = 0;
+            int total= 0;
+
+            for (int i=start; i<=end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+
+    }
+}
---
+++ Ch04/5_참조 매개변수.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 메서드 참조 매개변수 실습하기 p256
+ * 
+ * 
+ */
+namespace Ch04
+{
+    internal class _5_참조_매개변수
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 3;
+            int n3 = 0;
+            int n4 = 0;
+
+            MyDivider(n1, n2, ref n3, ref n4);
+
+            Console.WriteLine("몫:{0},나머지{1}",n3,n4);
+        }//end of Main
+
+        public static void MyDivider(int a1,int a2, ref int a3, ref int a4)
+        {
+            a3 = a1 / a2;
+            a4 = a1 % a2;
+
+        }
+    }
+}
