091e462c8155bfac3d05d9b9690ab6706b61d761
cjmj216 <cjmj216@gmail.com>
2022-07-14 17:18:20 +0900

    수업예제

--- Ch04/5_참조 매개변수.cs
+++ Ch04/5_참조 매개변수.cs
@@ -14,14 +14,17 @@ namespace Ch04
     {
         static void Main(string[] args)
         {
+
             int n1 = 10;
             int n2 = 3;
             int n3 = 0;
             int n4 = 0;
 
             MyDivider(n1, n2, ref n3, ref n4);
-
             Console.WriteLine("몫:{0},나머지{1}",n3,n4);
+        
+
+
         }//end of Main
 
         public static void MyDivider(int a1,int a2, ref int a3, ref int a4)
