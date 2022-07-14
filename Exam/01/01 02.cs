ce167c5a4ffe3a925dcfe3847b7b2bee0c3a802d
cjmj216 <cjmj216@gmail.com>
2022-07-14 17:16:47 +0900

    문제

---
+++ Exam/01/01 02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 2022/7/14
+ * 조민제
+ * 
+ */
+namespace Exam._01
+{
+    internal class _01_02
+    {
+        static void Main1(string[] args)
+        {
+            int year;
+            int birth;
+            string name;
+
+            Console.Write("올해 년도: ");
+            year = int.Parse(Console.ReadLine());
+            Console.Write("태어난 년도: ");
+            birth = Convert.ToInt32(Console.ReadLine);
+
+            Console.Write("이름: ");
+            name=Console.ReadLine();    
+
+            int age = year - birth;
+
+            Console.WriteLine("{0}님 안녕하세요\n당신은 올해 ");
+            
+
+            
+
+            
+        }  
+    }
+}
---
+++ Exam/01/01 04.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam._01
+{
+    internal class _01_04
+    {
+        static void Main1(string[] args)
+        {
+            int n = 1;
+            int result;
+
+            result = n++;
+            Console.WriteLine("result: " +result);
+
+            result = ++n;
+            Console.WriteLine("result: " + result);
+
+            result = n--;
+            Console.WriteLine("result: " + result);
+
+            result = --n;
+            Console.WriteLine("result: " + result);
+        }
+    }
+}
