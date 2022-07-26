31670c8ace8acd3f915938fed0bbc64de3f00e3a
cjmj216 <cjmj216@gmail.com>
2022-07-26 13:07:36 +0900

    수업예제

---
+++ Ch09/1_대리자.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 2022/7/26
+ * 조민제
+ * 대리자 실습하기 교재 493
+ * 
+ * 대리자(Delegate)
+ * - 대리자는 메서드에 대한 참조를 하기 위한 구문으로 메서드를 대신 호출 해주는 역활
+ * - 대리자를 익명함수로 많이 사용(최신 프로그래밍에서는 람다식으로 대체)
+ */
+namespace Ch09
+{
+    //대리자 선언
+    public delegate int MyDelegate(int x,int y);
+    public delegate int SumDelegate(int[] arr);
+
+    internal class _1_대리자
+    {
+        static void Main1(string[] args)
+        {
+            //대리자 초기화
+            MyDelegate my1 = new MyDelegate(Plus);
+            MyDelegate my2 = new MyDelegate(Miuns);
+
+            //대리자 실행
+            int r1 = my1(1, 2);
+            int r2 = my2(1, 2);
+
+            Console.WriteLine("r1: "+r1);
+            Console.WriteLine("r2: "+r2);
+
+            //대리자 활용: 매서드의 매개변수로 대리자 선언
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int rs1 = Sum(arr,OddSun);
+            int rs2 = Sum(arr,EvenSun);
+
+            Console.WriteLine("홀수합: " + rs1);
+            Console.WriteLine("짝수합: " + rs2);
+
+            //대리자 활용: 익명 메서드로 활용
+            var function = delegate (int x,int y)
+            {
+                return x + y;
+            };
+            int res1 = function(1, 2);
+            int res2 = function(2, 3);
+
+            Console.WriteLine("res1: " +res1);
+            Console.WriteLine("res2: " +res2);
+
+            //익명함수를 매개변수로 컬렉션 메서드
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            list.ForEach(delegate(int n)
+            {
+                Console.Write(n+ " ");
+            });
+            Console.WriteLine();
+
+            foreach(int x in list)
+            {
+                Console.Write(x+ " ");
+            }
+            Console.WriteLine();
+
+
+
+        }//Main end
+
+        public static int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Miuns(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int OddSun(int[] arr)
+        {
+            int sum = 0;
+
+            foreach(int n in arr)
+            {
+                if(n % 2 == 1)
+                {
+                    sum += n;
+                }
+            }
+            return sum;
+        }
+        public static int EvenSun(int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int n in arr)
+            {
+                if (n % 2 == 0)
+                {
+                    sum += n;
+                }
+            }
+            return sum;
+        }
+
+        public static int Sum(int[]arr, SumDelegate sd)
+        {
+            return sd(arr);
+        }
+
+    }
+}
---
+++ Ch09/3_Linq.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Linq 실습하기 교재 525
+ * 
+ * Linq
+ * - Linq(Languge Intergrated Querry)는 컬렉션을 대상으로 데이터를 처리하기 위한 C# 질의언어
+ * - Linq를 사용해서 DB와 함꼐 데이터를 쉽게 관리
+ */
+namespace Ch09
+{
+    class Member
+    {
+        private string uid;
+        private string name;
+        private int age;
+
+        public Member(string uid, string name, int age)
+        {
+            this.uid = uid;
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Uid { get => uid; set => uid = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+
+
+    internal class _3_Linq
+    {
+        static void Main(string[] args)
+        {
+            //켈렉션 생성
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //결과 리스트 생성
+            List<int> rslist1 = new List<int>();
+            List<int> rslist2 = new List<int>();
+
+            //for문 이용한 짝수 추출
+            foreach(int i in list)
+            {
+                if(i % 2 == 0)
+                {
+                    rslist1.Add(i);
+                }
+            }
+
+            //람다식을 이용한 짝수 추출
+            list.ForEach((n) => {
+                if (n % 2 == 0)
+                {
+                    rslist2.Add(n);
+                }
+            });
+
+
+            //Linq를 이용한 짝수 추출
+            var rslist3 =   from n in list
+                            where n % 2 == 0
+                            select n;
+
+            Console.WriteLine("rslist1: " +string.Join(",",rslist1));
+            Console.WriteLine("rslist2: " +string.Join(",",rslist2));
+            Console.WriteLine("rslist3: " +string.Join(",",rslist3));
+
+            //데이터가 5이상 큰 순서대로 추출
+            var rslist4 = from n in list
+                          where n >= 5
+                          orderby n descending
+                          select n;
+
+            Console.WriteLine("relist4: " + string.Join(",",rslist4));
+
+            //Linq 객체 리스트 활용
+            List<Member> members = new List<Member>();
+            members.Add(new Member("a101", "김유신", 25));
+            members.Add(new Member("a102", "김춘추", 23));
+            members.Add(new Member("a103", "장보고", 35));
+            members.Add(new Member("a104", "강감찬", 45));
+            members.Add(new Member("a105", "이순신", 55));
+
+            //30세 이상,이름,사전 순 추출
+            var rslist5 = from m in members
+                          where m.Age >= 30
+                          orderby m.Name ascending
+                          select m;
+
+            foreach(Member m in rslist5)
+            {
+                Console.WriteLine("{0},{1},{2}", m.Uid,m.Name,m.Age);
+            }
+
+
+
+
+
+
+        }
+    }
+}
